7e7d90b409468ae30038e1f0a42075f97816f15b
pe200012 <1326263755@qq.com>
2023-12-18 16:01:38 +0800

    loginresponse tostring

--- kotlinblog-backend/kt-authentication/src/main/kotlin/moe/saikyo47/domain/entity/LoginResponse.kt
+++ kotlinblog-backend/kt-authentication/src/main/kotlin/moe/saikyo47/domain/entity/LoginResponse.kt
@@ -1,6 +1,12 @@
 package moe.saikyo47.domain.entity
 
+import org.apache.logging.log4j.util.Base64Util
+
 class LoginResponse(accessToken: String, refreshToken: String) {
     var accessToken: String = accessToken
     var refreshToken: String = refreshToken
+
+    override fun toString(): String {
+        return "$accessToken,$refreshToken"
+    }
 }
