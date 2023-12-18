d969b0d03d4826f3ce3ebc6e8b685927449e24cb
pe200012 <1326263755@qq.com>
2023-12-18 16:01:56 +0800

    tokenservice fix refreshtoken

--- kotlinblog-backend/kt-authentication/src/main/kotlin/moe/saikyo47/service/TokenService.kt
+++ kotlinblog-backend/kt-authentication/src/main/kotlin/moe/saikyo47/service/TokenService.kt
@@ -17,9 +17,14 @@ class TokenService {
 
     public fun createToken(username: String, password: String): LoginResponse {
         val accessToken = createAccessToken(username)
-        return LoginResponse(accessToken, "refreshToken")
+        val refreshToken = createRefreshToken(username, password)
+        return LoginResponse(accessToken, refreshToken)
     }
 
     private fun createAccessToken(username: String): String =
         Jwts.builder().subject(username).signWith(Jwts.SIG.HS256.key().build()).compact()
+
+    private fun createRefreshToken(username: String, password: String): String =
+        Jwts.builder().subject(username+password).signWith(Jwts.SIG.HS256.key().build()).compact()
+
 }
