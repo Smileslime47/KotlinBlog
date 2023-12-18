022a39f5c60801d8c4af694a686d0ca9b9d44aa5
pe200012 <1326263755@qq.com>
2023-12-18 16:02:57 +0800

    require authentication for aritcle browsing

--- kotlinblog-backend/kt-authentication/src/main/kotlin/moe/saikyo47/config/SecurityConfig.kt
+++ kotlinblog-backend/kt-authentication/src/main/kotlin/moe/saikyo47/config/SecurityConfig.kt
@@ -5,6 +5,7 @@ import moe.saikyo47.enums.AppHttpCodeEnum
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod.GET
 import org.springframework.http.HttpMethod.POST
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.AuthenticationProvider
@@ -13,6 +14,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.NoOpPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
@@ -48,6 +50,8 @@ class SecurityConfig {
                     .permitAll()
                     .requestMatchers(POST, "/api/login")
                     .anonymous()
+                    .requestMatchers(GET, Constant.ApiPath.ARTICLE_API + Constant.ApiPath.ARTICLE_DETAIL_BY_ID)
+                    .authenticated()
                     .anyRequest()
                     // leave a dummy authentication lambda here
                     .permitAll()
