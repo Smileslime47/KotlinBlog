8f00ac2e2c912a243a3c566f62a70df650e321fc
pe200012 <1326263755@qq.com>
2023-12-18 15:20:49 +0800

    distribute authority for authenticated users

--- kotlinblog-backend/kt-authentication/src/main/kotlin/moe/saikyo47/domain/dto/LoginUser.kt
+++ kotlinblog-backend/kt-authentication/src/main/kotlin/moe/saikyo47/domain/dto/LoginUser.kt
@@ -1,6 +1,8 @@
 package moe.saikyo47.domain.dto
 
 import moe.saikyo47.domain.entity.User
+import moe.saikyo47.mapper.GroupMapper
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 
@@ -11,8 +13,38 @@ import org.springframework.security.core.userdetails.UserDetails
  * @since 2023-09-03
  */
 class LoginUser(val user: User) : UserDetails {
+    @Suppress("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    lateinit var groupMapper: GroupMapper
+
     override fun getAuthorities(): MutableCollection<out GrantedAuthority>? {
-        return null;
+        val authorities: MutableList<GrantedAuthority> = mutableListOf()
+        val group = groupMapper.selectById(user.permissionGroup)
+        if (group.permissionLogin == 1) {
+            authorities.add(GrantedAuthority { "LOGIN" })
+        }
+        if (group.permissionRelease == 1) {
+            authorities.add(GrantedAuthority { "RELEASE" })
+        }
+        if (group.permissionEdit == 1) {
+            authorities.add(GrantedAuthority { "EDIT" })
+        }
+        if (group.permissionDelete == 1) {
+            authorities.add(GrantedAuthority { "DELETE" })
+        }
+        if (group.permissionComment == 1) {
+            authorities.add(GrantedAuthority { "COMMENT" })
+        }
+        if (group.permissionAdminMonit == 1) {
+            authorities.add(GrantedAuthority { "ADMIN_MONIT" })
+        }
+        if (group.permissionAdminArticle == 1) {
+            authorities.add(GrantedAuthority { "ADMIN_ARTICLE" })
+        }
+        if (group.permissionAdminUser == 1) {
+            authorities.add(GrantedAuthority { "ADMIN_USER" })
+        }
+        return authorities
     }
 
     override fun getPassword(): String = user.password!!
